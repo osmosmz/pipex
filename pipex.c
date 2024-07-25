cecc92fda9186f897c0c95b432b93344b19ba8e4
mzhuang <mervynzhuang@gmail.com>
2024-07-26 01:38:44 +0800

    todo: parser for commands

--- pipex.c
+++ pipex.c
@@ -6,48 +6,82 @@
 /*   By: mzhuang <mzhuang@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 18:19:54 by mzhuang           #+#    #+#             */
-/*   Updated: 2024/07/21 16:12:39 by mzhuang          ###   ########.fr       */
+/*   Updated: 2024/07/26 01:38:11 by mzhuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h> // comment away after
 
-char *parsepath(char **envp)
-{	
-	char *result;
-	int i;
-	i=0;
+
+void parsecmds(t_cmd *cmds, char **envp, char**av)
+{
+	// todo
+}
+char	*parsepath(char **envp)
+{
+	int	i;
+
+	i = 0;
 	while (envp[i])
 	{
-		if(ft_substr())
+		if (ft_strnstr(envp[i], "PATH", 4))
+			return (ft_strdup(envp[i]));
 		i++;
 	}
-
+	return (NULL);
 }
 
+// parser function to inintialise all cmds
+// cmds is a struct with cmd number (1,2,3 etc)
+// its cmd args
+// its bin
 
+char	*getpaths(char **envp, char **argv, int cmdno)
+{
+	char	*path;
+	char	**paths;
+	char	**cmdargs;
+	char	*finalpath;
+	char	*bin;
+
+	path = parsepath(envp);
+	paths = ft_split(&path[5], ':');
+	cmdargs = ft_split(argv[cmdno + 1], ' ');
+	free(path);
+	while (*paths)
+	{
+		finalpath = ft_strjoin(*paths, "/");
+		bin = ft_strjoin(finalpath, cmdargs[0]);
+		free(finalpath);
+		if (access(bin, F_OK))
+			return (bin);
+		(paths)++;
+	}
+	return (NULL);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
-	(void) argc ;
-	(void) argv;
-	// if (argc < 5)
-	// {
-	// 	ft_printf("Usage: ./pipex infile cmd1 cmd2 outfile");
-	// 	return (1);
-	// }
+	(void)argc;
+	(void)argv;
+	t_cmd cmds[2];
+	if (argc < 5)
+	{
+		ft_printf("Usage: ./pipex infile cmd1 cmd2 outfile");
+		return (1);
+	}
+	parsecmds(cmds,envp,argv);
 	// int readfd;
 	// int savefd;
-
-	// char * string;
-	while (envp)
-	{
-		printf("%s\n", *envp);
-		envp++;
-	}
-
 	// readfd = open(argv[1], O_RDONLY);
 	// savefd = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	getpaths(envp, argv, 1);
+	// while (envp)
+	// {
+	// 	printf("%s\n", *envp);
+	// 	envp++;
+	// }
+
 	// if (readfd < 0 || savefd < 0 )
 }
--- pipex.h
+++ pipex.h
@@ -6,15 +6,21 @@
 /*   By: mzhuang <mzhuang@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 18:20:08 by mzhuang           #+#    #+#             */
-/*   Updated: 2024/07/20 18:34:45 by mzhuang          ###   ########.fr       */
+/*   Updated: 2024/07/26 01:31:41 by mzhuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
-#define PIPEX_H
-#include <fcntl.h>
-#include "./libft/libft.h"
+# define PIPEX_H
+# include "./libft/libft.h"
+# include <fcntl.h>
 
+typedef struct s_cmd
+{
+	char	*bin;
+	char	**argv;
+	int		cmdnumber;
 
+}			t_cmd;
 
 #endif
